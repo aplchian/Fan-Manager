3817c711454fe787f05aaa5598acf5d15aeca8ca
aplchian <alex.boquis@gmail.com>
2016-12-27 01:40:56 -0500

    moved web1

--- web/src/App.js
+++ web/src/App.js
@@ -1,62 +1,90 @@
-import React, { Component } from 'react';
-const {
-  Router,
-  Route,
-  hashHistory,
-  Link
-} = require('react-router')
-const syncDB = require('./helpers/syncDB.js')
-const h = require('react-hyperscript')
-const Home = require('./home.js')
-const xhr = require('xhr')
-const NavLink = require('./components/NavLink.js')
+const React = require('react')
+const {Match,HashRouter,Redirect} = require('react-router')
+const {style,insertRule,merge} = require('glamor')
+const Home = require('./pages/home')
+const FanDashBoard = require('./pages/fans/dashboard')
+const AddFan = require('./pages/fans/add')
+const EditFan = require('./pages/fans/add')
+const ShowFan = require('./pages/fans/show')
+const FanSearch = require('./pages/fans/search')
+const StreetTeam = require('./pages/fans/streetteam')
+const Service = require('./services/services')
+import Manage from './pages/manage'
+import DaySheets from './pages/manage/daysheets'
+import AddEvent from './pages/manage/add-event'
+import AddTodo from './pages/manage/todo-form'
+import AddDaySheet from './pages/manage/add-daysheet'
+import Events from './pages/manage/events'
+import Event from './pages/manage/show-event'
+import DaySheet from './pages/manage/show-daysheet'
+import Todos from './pages/manage/todos'
+const auth = require('./utils/auth')(
+  process.env.REACT_APP_ID,
+  process.env.REACT_APP_DOMAIN
+)
 
 
-
-module.exports = React.createClass({
-  getInitialState: function(){
+const App = React.createClass({
+  getInitialState(){
     return ({
-      syncClass: 'fr mr5 pointer red'
+      user: null
     })
   },
-  componentDidMount: function(){
-    syncDB.load()
+  logOut(){
+    this.setState({
+      loggedOut: true
+    })
   },
-  sync: function(){
-    var success = syncDB.sync()
-    if(success = 'success'){
-      this.setState({
-        syncClass: 'fr mr5 pointer green'
-      })
+  setUser(user){
+    this.setState({user})
+  },
+  setBand(band){
+    return () => {
+      this.setState({band})
     }
   },
-  render: function(){
-    return (
-      <div>
-      <header>
-        <nav>
-          <div className="logo"/>
-          <ul className="bb b--light-gray w-90 db center pb2" role="nav">
-            <NavLink title="Fans" path="/"/>
-          </ul>
-        </nav>
-      </header>
-      <div className="sub-menu">
-        <ul>
-          <li>
-            <Link to="/search">Search</Link>
-          </li>
-          <li>
-            <Link to="/streetteam">Street Team</Link>
-          </li>
-          <li>
-            <Link to="/addfan">Add Fan</Link>
-          </li>
-        </ul>
-      </div>
-      {this.props.children}
-      <footer/>
-      </div>
+  setBands(bands){
+    console.log('bands',bands)
+  },
+  render(){
+    return(
+      <HashRouter>
+        <div>
+          <Match exactly pattern="/" render={props => <Home auth={auth} setBands={this.setBands} setBand={this.setBand} setUser={this.setUser} {...props} />} />
+          <MatchWhenAuthorized exactly pattern="/fans" component={Service(FanDashBoard,this.logOut,this.state)}></MatchWhenAuthorized>
+          <MatchWhenAuthorized exactly pattern="/:type/fans" component={Service(FanSearch,this.logOut,this.state)}></MatchWhenAuthorized>
+          <MatchWhenAuthorized exactly pattern="/fans/add" component={Service(AddFan,this.logOut,this.state)}></MatchWhenAuthorized>
+          <MatchWhenAuthorized exactly pattern="/fans/:id/show" component={Service(ShowFan,this.logOut,this.state)}></MatchWhenAuthorized>
+          <MatchWhenAuthorized exactly pattern="/fans/:id/edit" component={Service(AddFan,this.logOut,this.state)}></MatchWhenAuthorized>
+
+          <MatchWhenAuthorized exactly pattern="/manage" logout={this.logOut} component={Service(Manage,this.logOut,this.state)}></MatchWhenAuthorized>
+          <MatchWhenAuthorized exactly pattern="/manage/events" component={Service(Events,this.logOut,this.state)}></MatchWhenAuthorized>
+          <MatchWhenAuthorized exactly pattern="/manage/daysheets" logout={this.logOut} component={Service(DaySheets,this.logOut,this.state)}></MatchWhenAuthorized>
+
+          <MatchWhenAuthorized exactly pattern="/manage/daysheets/:id/edit" component={Service(AddDaySheet,this.logOut,this.state)}></MatchWhenAuthorized>
+          <MatchWhenAuthorized exactly pattern="/manage/daysheets/:id/show" component={Service(DaySheet,this.logOut,this.state)}></MatchWhenAuthorized>
+          <MatchWhenAuthorized exactly pattern="/manage/daysheets/add" component={Service(AddDaySheet,this.logOut,this.state)}></MatchWhenAuthorized>
+
+          <MatchWhenAuthorized exactly pattern="/manage/events/add" component={Service(AddEvent,this.logOut,this.state)}></MatchWhenAuthorized>
+          <MatchWhenAuthorized exactly pattern="/manage/events/:id/show" component={Service(Event,this.logOut,this.state)}></MatchWhenAuthorized>
+          <MatchWhenAuthorized exactly pattern="/manage/events/:id/edit" component={Service(AddEvent,this.logOut,this.state)}></MatchWhenAuthorized>
+
+          <MatchWhenAuthorized exactly pattern="/manage/todos" component={Service(Todos,this.logOut,this.state)}></MatchWhenAuthorized>
+          <MatchWhenAuthorized exactly pattern="/manage/todos/add" component={Service(AddTodo,this.logOut,this.state)}></MatchWhenAuthorized>
+          <MatchWhenAuthorized exactly pattern="/manage/todos/:id/edit" component={Service(AddTodo,this.logOut,this.state)}></MatchWhenAuthorized>
+
+        </div>
+      </HashRouter>
     )
   }
 })
+
+const MatchWhenAuthorized = ({component: Component, ...rest}) =>
+  <Match {...rest} render={props => auth.loggedIn() ?
+    <div>
+       <Component {...props} />
+     </div> : <Redirect to="/" />
+  }
+/>
+
+module.exports = App
--- web/src/index.js
+++ web/src/index.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import { render } from 'react-dom'
-import App from './App';
-import {Router, Route, hashHistory, browserHistory, IndexRoute} from 'react-router'
-import h from 'react-hyperscript'
-import Home from './home.js'
-import Search from './resources/pages/Search'
-import StreetTeam from './resources/pages/StreetTeam'
-import Fan from './resources/pages/Fan'
-import AddFan from './resources/pages/AddFan'
-import EditFan from './resources/pages/EditFan'
+import ReactDOM from 'react-dom';
+import '../public/styles/custom.css';
+const App = require('./App')
+const {Match,BrowserRouter} = require('react-router')
 
-render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
-      <Route path="/search" component={Search}/>
-      <Route path="/fans" component={Search}/>
-      <Route path="/streetteam" component={StreetTeam}/>
-      <Route path="/fan/:id" component={Fan}/>
-      <Route path="/fan/edit/:id" component={EditFan}/>
-      <Route path="/addfan" component={AddFan}/>
-    </Route>
-  </Router>
-),document.getElementById('root'))
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
