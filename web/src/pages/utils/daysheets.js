d1782bb18fef05d7d2641c3fe0ec74fc0cdd2af9
aplchian <alex.boquis@gmail.com>
2017-06-08 20:58:36 -0400

    added time picker to daysheet add

--- web/src/pages/utils/daysheets.js
+++ web/src/pages/utils/daysheets.js
@@ -1,8 +1,9 @@
 const moment = require('moment')
-const {sort, compose, map, curry} = require('ramda')
-const {Link} = require('react-router')
-import {Panel,Checkbox,FormControl,Button} from 'react-bootstrap'
+const { sort, compose, map, curry, identity } = require('ramda')
+const { Link } = require('react-router')
+import { Panel, Checkbox, FormControl, Button } from 'react-bootstrap'
 var FontAwesome = require('react-fontawesome')
+const { Maybe } = require('ramda-fantasy')
 import React from 'react'
 
 const sortAsc = compose(
@@ -15,58 +16,60 @@ const sortDesc = compose(
   sort(desc)
 )
 
-function strToMoment(x){
+function strToMoment(x) {
   return moment(x.date.split('T')[0])
 }
 
-function datetoUnix(x){
+function datetoUnix(x) {
   return strToMoment(x).unix()
 }
 
-function asc(a,b){
- return datetoUnix(a) - datetoUnix(b)
+function asc(a, b) {
+  return datetoUnix(a) - datetoUnix(b)
 }
 
-function desc(a,b){
- return datetoUnix(b) - datetoUnix(a)
+function desc(a, b) {
+  return datetoUnix(b) - datetoUnix(a)
 }
 
-function showResults(item,i){
+function showResults(item, i) {
   let date = strToMoment(item).format('MMM DD')
   return <Link to={`/manage/daysheets/${item._id}/show`}><Panel className="daysheet-panel" key={i}><FontAwesome name='sun-o' /> {date}</Panel></Link>
 
 }
 
-function sortBy(order,results){
+function sortBy(order, results) {
   return order === "asc" ? sortAsc(results) : sortDesc(results)
 }
 
-function sortIcon(order){
+function sortIcon(order) {
   return order === "asc"
-          ? <FontAwesome className="sort-icon-asc daysheet-icon" name='sort-asc' />
-          : <FontAwesome className="sort-icon daysheet-icon" name='sort-desc' />
+    ? <FontAwesome className="sort-icon-asc daysheet-icon" name='sort-asc' />
+    : <FontAwesome className="sort-icon daysheet-icon" name='sort-desc' />
 }
 
 //// daysheet form
 
-const listEvents = curry(function(fn,item){
+const listEvents = curry(function (fn, item) {
   return item.status === 'confirmed'
-          ? <Checkbox key={item.id} checked onChange={fn(item.id)}>{item.name}</Checkbox>
-          : <Checkbox key={item.id} onChange={fn(item.id)}>{item.name}</Checkbox>
+    ? <Checkbox key={item.id} checked onChange={fn(item.id)}>{item.name}</Checkbox>
+    : <Checkbox key={item.id} onChange={fn(item.id)}>{item.name}</Checkbox>
 })
 
-const showEvents = curry(function(fn,item){
-  return <FormControl.Static className="form-item-container">
-          <span className="form-item-title">
-            {`${item.event}`}
-          </span>
-            <Button className="pull-right remove-btn" onClick={fn(item.id)}>remove</Button>
-          </FormControl.Static>
-})
+const getName = ({ event }) => Maybe(event).map(identity)
 
- module.exports = {
-  sortBy,
-  sortIcon,
-  listEvents,
-  showEvents
- }
+const showEvents = curry((fn, item) => (
+  <FormControl.Static className="form-item-container">
+    <span className="form-item-title">
+      {`${getName(item).getOrElse('Unnamed')} - ${moment(item.starttime, 'HH:mm').format('h:mm a')}`}
+    </span>
+    <Button className="pull-right remove-btn" onClick={fn(item.id)}>remove</Button>
+  </FormControl.Static>
+))
+
+module.exports = {
+    sortBy,
+    sortIcon,
+    listEvents,
+    showEvents
+  }
